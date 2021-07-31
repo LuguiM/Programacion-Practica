e30f1414627a6dd4f159fd752d12db03fdf52497
LuguiM <88117946+LuguiM@users.noreply.github.com>
2021-07-31 15:12:44 -0600

    Add files via upload

---
+++ Creacion_de_formularios_1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Creacion_de_formularios_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void wiiiButton_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Hello World! " +
+                "Katerin Estela Orellana Hernandez, " +
+                "Rigoberto Alexander Mejia Sorto, " +
+                "Jose Luis Muñoz Padilla, " +
+                "Kerin Francisco Melendez Mejia, " +
+                "Maria del Carmen Rodriguez Martinez, " +
+                "Angel Ernesto Aparicio Martinez";
+        }
+    }
+}
---
+++ Practica_de_consola_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables y inicialize en cero.
+            int num1 = 0; int num2 = 0;
+
+            // Mostrar titulo que es calculadora de consola de C#
+            Console.WriteLine("Calculadora de consola en C#\r");
+            Console.WriteLine("------------------------\n");
+
+            // Pedirle el primer numero al usuario.
+            Console.WriteLine("Escribe un numero, y preciona Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Pedirle el segundo numero al usuario
+            Console.WriteLine("Escribe otro numero, y preciona Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Preguntarle al usuario la opcion a elegir.
+            Console.WriteLine("Escogser una opcion del siguiente listado:");
+            Console.WriteLine("\ts - Suma");
+            Console.WriteLine("\tr - Resta");
+            Console.WriteLine("\tm - Multiplicacion");
+            Console.WriteLine("\td - Divicion");
+            Console.Write("Cual es tu opcion? ");
+
+            // Use switch para que realice la operacion matematica
+            switch (Console.ReadLine())
+            {
+                case "s":
+                    Console.WriteLine($"Tu resultado es: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "r":
+                    Console.WriteLine($"Tu resultado es: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Tu resultado es: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    Console.WriteLine($"Tu resultado es: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+            // Espere que el usuario responda para despues cerrar.
+            Console.Write("Presiona cualquier tecla para cerrar la calculadora de consola...");
+            Console.ReadKey();
+        }
+    }
+}
